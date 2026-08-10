41e0f7efb902f4e2ee1a8339be8d554f20a32fd1
Bolaji Adeyemi <d.bola1@gmail.com>
2026-08-10 08:26:57 +0100

    add openai integration

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 
 from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
@@ -8,6 +10,30 @@ load_dotenv()
 def main():
     print("Hello from langchain-course!")
     print(os.environ.get("OPENAI_API_KEY"))
+    information = """
+    Elon Reeve Musk (/ˈiːlɒn/ ⓘ EE-lon; born June 28, 1971) is a businessman and former public official who is the CEO and largest shareholder of Tesla and SpaceX. Musk has been the wealthiest person in the world since 2025, and became the only trillionaire in terms of US dollars in June 2026; as of August 8, 2026, Forbes estimates his net worth to be US$823 billion.
+
+    Born into the wealthy Musk family in Pretoria, South Africa, Musk emigrated in 1989 to Canada; he has Canadian citizenship since his mother was born there. He received bachelor's degrees in 1997 from the University of Pennsylvania before moving to California to pursue business ventures. In 1995, Musk co-founded Zip2, a web software company. Following its sale in 1999, he co-founded X.com, an e-commerce payment system that merged with Confinity in March 2000 to form PayPal, which was acquired by eBay in 2002. Musk also became an American citizen in 2002.
+    """
+    summary_template = """
+    given the information {information} about a person I want you to create:
+    1. A short summary
+    2. two interesting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    # llm = ChatOllama(temperature=0, model="gemma3:270m")
+
+    llm = ChatOpenAI(temperature=0, model="gpt-5")
+
+    chain = summary_prompt_template | llm
+
+    response = chain.invoke(input={"information": information})
+
+    print(response.content)
 
 
 if __name__ == "__main__":
